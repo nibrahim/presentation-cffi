b85fb7ef5aac9892a1ef1943bcd54b596b772705
Noufal Ibrahim <noufal@nibrahim.net.in>
2016-09-08 12:44:31 +0530

    Tighter formatting.

--- code/leftpad_cffi_build.py
+++ code/leftpad_cffi_build.py
@@ -7,10 +7,7 @@ ffi.set_source('pyleftpad', '',
                libraries=["leftpad"],
                library_dirs=['.'])
 
-ffi.cdef("""
-char *left_pad_string(char *ip, size_t ip_count, size_t pad_count);
-""")
-
+ffi.cdef("char *left_pad_string(char *ip, size_t ip_count, size_t pad_count);")
 
 if __name__ == '__main__':
     ffi.compile()
