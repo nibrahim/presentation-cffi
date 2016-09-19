4fb7632c15d6bee185e932125670ed1d39e1ba10
Noufal Ibrahim <noufal@nibrahim.net.in>
2016-09-19 12:51:00 +0530

    Adds main block

--- code/leftpad_cffi_api.py
+++ code/leftpad_cffi_api.py
@@ -1,8 +1,10 @@
 import pyleftpad
 
-
 def leftpad(ip):
     ip = ip.encode('ascii')
     op = pyleftpad.lib.left_pad_string(ip, len(ip), 20)
     return pyleftpad.ffi.string(op).decode('ascii')
+
+if __name__ == '__main__':
+    print ("'{}'".format(leftpad("python")))
 
