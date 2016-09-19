ca57ac5bd1516b4204b92ced40e9a089dcb69370
Noufal Ibrahim <noufal@nibrahim.net.in>
2016-09-19 11:51:08 +0530

    Finicky

--- code/leftpad.c
+++ code/leftpad.c
@@ -4,19 +4,19 @@
 #include <stdlib.h>
 #include <string.h>
 
-char *
-left_pad_string(char *ip, size_t ip_count, size_t pad_count)
+char * left_pad_string(char *ip, size_t ip_count, size_t pad_count)
 {
   char * ret;
-  uint32_t i, j;
-  ret = (char *)calloc(pad_count, sizeof(char));
-  for (i=0; i<(pad_count - ip_count); i++) {
-    ret[i] = ' ';
+  uint32_t i;
+  if (pad_count < ip_count) {
+    ret = strndup(ip, ip_count);
+  } else {
+    ret = (char *)calloc(pad_count+ip_count, sizeof(char));
+    for (i=0; i<(pad_count - ip_count); i++) {
+      ret[i] = ' ';
+    }
+    strncat(ret, ip, ip_count);
   }
-  for (j=0; j<ip_count; j++, i++) {
-    ret[i] = ip[j];
-  }
-  ret[i] = '\0';
   return ret;
 }
 
@@ -24,7 +24,7 @@ int
 main()
 {
   char ip[] = "python";
-  char *op = left_pad_string(ip, strlen(ip), 15);
+  char *op = left_pad_string(ip, strlen(ip), 25);
   printf("'%s'\n", op);
   free(op);
   return 0;
